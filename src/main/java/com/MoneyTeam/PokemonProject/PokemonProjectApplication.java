dfebdc1187ba26d5253abc995955958761842d8a
Jason Cothran <healingfox115@gmail.com>
2021-09-10 15:18:19 -0500

    Initial commit

---
+++ src/main/java/com/MoneyTeam/PokemonProject/PokemonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.MoneyTeam.PokemonProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MoneyTeam/PokemonProject/PokemonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MoneyTeam.PokemonProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
