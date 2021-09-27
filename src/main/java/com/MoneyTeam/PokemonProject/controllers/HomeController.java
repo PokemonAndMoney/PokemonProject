1e3cbb1830b76ad6f72e4ec05616c112b12b6a0d
ishmael214 <64385652+ishmael214@users.noreply.github.com>
2021-09-27 16:21:21 -0500

    added initial api call by pokemon name

--- src/main/java/com/MoneyTeam/PokemonProject/controllers/HomeController.java
+++ src/main/java/com/MoneyTeam/PokemonProject/controllers/HomeController.java
@@ -1,15 +1,36 @@
 package com.MoneyTeam.PokemonProject.controllers;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@Controller
+@RestController
 public class HomeController extends BaseController {
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	private static String pokemonUrl = "https://pokeapi.co/api/v2/pokemon/";
 	
 	@RequestMapping("/") // @RequestParam go/to/my/page?r=1
 	public String home() {
 		return "index.jsp";
 	}
+	@GetMapping("/{name}")
+	public Object getPokemon(@PathVariable("name") String name){
+		LinkedHashMap pokemon =  restTemplate.getForObject(pokemonUrl + name, LinkedHashMap.class);
+//		return pokemon.get("abilities");
+		return pokemon;
+	}
+	
 }
